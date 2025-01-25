0fa513326a521b4a7853618a4feb891cc7f2c11e
btzrmn <18496383+btzrmn@users.noreply.github.com>
2025-01-25 11:46:28 +0800

    Update Main.java

--- src/main/java/mn/mlc/Main.java
+++ src/main/java/mn/mlc/Main.java
@@ -2,6 +2,6 @@ package src.main.java.mn.mlc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello MLC");
     }
 }
