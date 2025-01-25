9077c3a5d744dc8b25dc6aa7ca0f2d2eddd186e1
batzorig <batzorig.b@nomadicss.mn>
2025-01-25 11:55:15 +0800

    main java updated

--- src/main/java/mn/mlc/Main.java
+++ src/main/java/mn/mlc/Main.java
@@ -1,7 +1,11 @@
 package src.main.java.mn.mlc;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello MLC");
+        System.out.println("Hello Batzorig!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: Batzorig");
     }
 }
