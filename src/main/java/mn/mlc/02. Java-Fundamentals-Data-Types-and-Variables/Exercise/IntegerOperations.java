1310cc159c39151a0209dc3b836c6cb2b865e01d
batzorig <batzorig.b@nomadicss.mn>
2025-02-04 15:25:33 +0800

    exercises

---
+++ src/main/java/mn/mlc/02. Java-Fundamentals-Data-Types-and-Variables/Exercise/IntegerOperations.java
@@ -0,0 +1,17 @@
+package src.main.java.mn.mlc;
+
+import java.util.Scanner;
+
+public class IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+        int d = Integer.parseInt(scanner.nextLine());
+
+        System.out.printf("%d", ((a+b)/c)*d);
+
+    }
+}
---
+++ src/main/java/mn/mlc/02. Java-Fundamentals-Data-Types-and-Variables/Lab/CharsToString.java
@@ -0,0 +1,11 @@
+package src.main.java.mn.mlc;
+
+import java.util.Scanner;
+
+public class CharsToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine()+scanner.nextLine()+scanner.nextLine();
+        System.out.println(data);
+    }
+}
