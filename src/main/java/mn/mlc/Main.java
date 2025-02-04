ff217fb882e3b6c5ef3699fd40161dd792c7476a
batzorig <batzorig.b@nomadicss.mn>
2025-02-04 21:38:52 +0800

    exercises

--- src/main/java/mn/mlc/Main.java
+++ src/main/java/mn/mlc/Main.java
@@ -1,11 +1,10 @@
 package src.main.java.mn.mlc;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Batzorig branch test!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: Batzorig");
+    }
+
+    public static void printHello() {
+        System.out.println("Hello");
     }
 }
